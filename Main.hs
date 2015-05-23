d3e3db136ac84d6148e026f260b4e026e7a13160
Milan Nagy <123.wizek@gmail.com>
2015-05-23 22:57:18 +0200

    fixed reverse bug plus experimenting with DI

--- Main.hs
+++ Main.hs
@@ -3,18 +3,18 @@
 import Data.List
 
 class PType r where
-  p' :: [String] -> r
+  p' :: (String -> (IO ())) -> [String] -> r
 
 instance (a ~ ()) => PType (IO a) where
-  p' strings = putStrLn (reverse $ concatList strings)
+  p' f strings = f (concatList $ reverse strings)
     where concatList = intercalate " "
 
 instance (Show a, PType r) => PType (a -> r) where
-  p' strings = \x -> p' $ (show x : strings)
+  p' f strings = \x -> p' f $ (show x : strings)
 
 p :: (Show a, PType r) => a -> r
-p = p' []
+p = p' putStrLn []
 
 main = do
   p "a"
-  p "a" 1
+  p "ab" 1 2 32
