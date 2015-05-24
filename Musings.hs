b6fb85df1e82dcfb488078ca6c157bc3b58229a0
Milan Nagy <123.wizek@gmail.com>
2015-05-24 12:06:03 +0200

    line 30 shouldn't execute still

--- Musings.hs
+++ Musings.hs
@@ -5,7 +5,13 @@ import Data.Either
 
 data OneOrTwo = One (Int -> Int) | Two (Int -> Int -> Int)
 
-instance Show (Int -> Int) where show _ = "asd"
+fromOne (One x) = x
+fromTwo (Two x) = x
+
+-- fromVar (One x) = x
+-- fromVar (Two x) = x
+
+instance Show (Int -> Int) where show _ = "->"
 
 varArgs 1 = One (\x -> x)
 varArgs 2 = Two (\x y -> x + y)
@@ -15,10 +21,14 @@ fn 1 = Left "a"
 fn 2 = Right "b"
 
 main = do
-  print $ case fn 1 of Left x -> x
-  print $ case fn 1 of Right x -> x
+  -- print $ case fn 1 of Left x -> x
+  -- print $ case fn 1 of Right x -> x
   -- print $ case varArgs 1 of Two fn -> fn 3
   -- print $ let One fn = varArgs 1 in fn 3
+  print $ (fromOne $ varArgs 1) 3
+  -- print $ (fromOne $ varArgs 2) 3
+  print $ (fromTwo $ varArgs 1) 3
+  print $ (fromTwo $ varArgs 2) 3 4
   -- print $ let Two fn = varArgs 2 in fn 3 4
   -- print $ let One fn = varArgs 2 in fn 3 4  -- shouldn't execute
   -- print $ let Two fn = varArgs 1 in fn 3  -- shouldn't execute
