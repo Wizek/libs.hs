e114c0de3d9e215a9cb18c6af65ddb276fe8fe73
Milan Nagy <123.wizek@gmail.com>
2015-05-24 12:52:08 +0200

    self-contained example

--- Musings.hs
+++ Musings.hs
@@ -1,34 +1,19 @@
+-- This pragma + instance declaration is only to prevent an unrelated error
+-- "No instance for (Show (Int -> Int)) arising from a use of ‘print’"
 {-# LANGUAGE FlexibleInstances #-}
+instance Show (Int -> Int) where show _ = "->"
+-- Below is the relevant code:
+
 -- Variable argument length experiment
 
-import Data.Either
-
 data OneOrTwo = One (Int -> Int) | Two (Int -> Int -> Int)
-
-fromOne (One x) = x
-fromTwo (Two x) = x
-
--- fromVar (One x) = x
--- fromVar (Two x) = x
-
-instance Show (Int -> Int) where show _ = "->"
 
 varArgs 1 = One (\x -> x)
 varArgs 2 = Two (\x y -> x + y)
 
-
-fn 1 = Left "a"
-fn 2 = Right "b"
-
 main = do
-  -- print $ case fn 1 of Left x -> x
-  -- print $ case fn 1 of Right x -> x
-  -- print $ case varArgs 1 of Two fn -> fn 3
-  -- print $ let One fn = varArgs 1 in fn 3
-  print $ (fromOne $ varArgs 1) 3
-  -- print $ (fromOne $ varArgs 2) 3
-  print $ (fromTwo $ varArgs 1) 3
-  print $ (fromTwo $ varArgs 2) 3 4
-  -- print $ let Two fn = varArgs 2 in fn 3 4
+  print $ let One fn = varArgs 1 in fn 3
+  print $ let Two fn = varArgs 2 in fn 3 4
   -- print $ let One fn = varArgs 2 in fn 3 4  -- shouldn't execute
-  -- print $ let Two fn = varArgs 1 in fn 3  -- shouldn't execute
+  print $ let Two fn = varArgs 1 in fn 3  -- shouldn't execute
+
