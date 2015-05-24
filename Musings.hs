112f7c3b97e4c1edd6441542f2d4923810ed1e22
Milan Nagy <123.wizek@gmail.com>
2015-05-24 11:59:10 +0200

    How come this executes??

--- Musings.hs
+++ Musings.hs
@@ -1,14 +1,16 @@
-
+{-# LANGUAGE FlexibleInstances #-}
 -- Variable argument length experiment
 
 data OneOrTwo = One (Int -> Int) | Two (Int -> Int -> Int)
+
+instance Show (Int -> Int) where show _ = "asd"
 
 varArgs 1 = One (\x -> x)
 varArgs 2 = Two (\x y -> x + y)
 
 main = do
-  print $ case varArgs 1 of One fn -> fn 3
-  print $ let One fn = varArgs 1 in fn 3
-  print $ let Two fn = varArgs 2 in fn 3 4
-  print $ let One fn = varArgs 2 in fn 3 4  -- shouldn't execute
+  -- print $ case varArgs 1 of One fn -> fn 3
+  -- print $ let One fn = varArgs 1 in fn 3
+  -- print $ let Two fn = varArgs 2 in fn 3 4
+  -- print $ let One fn = varArgs 2 in fn 3 4  -- shouldn't execute
   print $ let Two fn = varArgs 1 in fn 3  -- shouldn't execute
