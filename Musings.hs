5dc56bc5a57c0e71538bed51b6c488e20b1e6bee
Milan Nagy <123.wizek@gmail.com>
2015-05-24 12:00:41 +0200

    The second line fails fortunately, too

--- Musings.hs
+++ Musings.hs
@@ -1,6 +1,8 @@
 {-# LANGUAGE FlexibleInstances #-}
 -- Variable argument length experiment
 
+import Data.Either
+
 data OneOrTwo = One (Int -> Int) | Two (Int -> Int -> Int)
 
 instance Show (Int -> Int) where show _ = "asd"
@@ -9,11 +11,12 @@ varArgs 1 = One (\x -> x)
 varArgs 2 = Two (\x y -> x + y)
 
 
-fn 1 = True
-fn 2 = False
+fn 1 = Left "a"
+fn 2 = Right "b"
 
 main = do
-  print $ case fn 1 of False -> "??"
+  print $ case fn 1 of Left x -> x
+  print $ case fn 1 of Right x -> x
   -- print $ case varArgs 1 of Two fn -> fn 3
   -- print $ let One fn = varArgs 1 in fn 3
   -- print $ let Two fn = varArgs 2 in fn 3 4
