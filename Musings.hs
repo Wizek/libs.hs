074f7516b3c42ba211c491f4586c14b2a6771b79
Milan Nagy <123.wizek@gmail.com>
2015-05-24 11:59:41 +0200

    This rightfully fails

--- Musings.hs
+++ Musings.hs
@@ -8,9 +8,14 @@ instance Show (Int -> Int) where show _ = "asd"
 varArgs 1 = One (\x -> x)
 varArgs 2 = Two (\x y -> x + y)
 
+
+fn 1 = True
+fn 2 = False
+
 main = do
-  -- print $ case varArgs 1 of One fn -> fn 3
+  print $ case fn 1 of False -> "??"
+  -- print $ case varArgs 1 of Two fn -> fn 3
   -- print $ let One fn = varArgs 1 in fn 3
   -- print $ let Two fn = varArgs 2 in fn 3 4
   -- print $ let One fn = varArgs 2 in fn 3 4  -- shouldn't execute
-  print $ let Two fn = varArgs 1 in fn 3  -- shouldn't execute
+  -- print $ let Two fn = varArgs 1 in fn 3  -- shouldn't execute
